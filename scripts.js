0755bc33aa10199f41f95d24b945855b84ff5115
raquelferreira1 <raquel.riosferreira@gmail.com>
2022-12-13 17:54:33 -0300

    adicionando projeto

---
+++ scripts.js
@@ -0,0 +1,68 @@
+const button = document.getElementById('botão-conversão') //peguei o click do botão 'converter'
+const select = document.getElementById('conversor-select')
+
+const dolar = 5.32 // valor atual do dólar
+const euro = 5.65
+const bitcoin = 0000011
+
+const convertValues = () => {
+    const inputReais = document.getElementById('input-valor').value
+    const valorReal = document.getElementById('valor-real')
+    const valorEmDolar = document.getElementById('valor-em-dolar')
+
+    valorReal.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    }).format(inputReais)
+
+    if (select.value === 'US$ Dolár Americano') {
+
+        valorEmDolar.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(inputReais / dolar)
+    }
+
+    if (select.value === '€ Euro') {
+
+        valorEmDolar.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(inputReais / euro)
+    }
+
+    if (select.value === '₿ Bitcoin') {
+
+        valorEmDolar.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC",
+        }).format(inputReais / bitcoin)
+    }
+}
+
+changeMudancaValor = () => {
+    const currencyName = document.getElementById('currency-name')
+    const currencyImg = document.getElementById('currency-img')
+
+    if (select.value === 'US$ Dolár Americano') {
+        currencyName.innerHTML = 'Dolár Americano'
+        currencyImg.src = './img/estados-unidos.png'
+    }
+
+    if (select.value === '€ Euro') {
+        currencyName.innerHTML = 'Euro'
+        currencyImg.src = './img/euro.png'
+    }
+
+    if (select.value === '₿ Bitcoin') {
+        currencyName.innerHTML = 'Bitcoin'
+        currencyImg.src = './img/bitcoin.png'
+    }
+
+    convertValues()
+}
+
+button.addEventListener('click', convertValues)
+select.addEventListener('change', changeMudancaValor)
+
+
