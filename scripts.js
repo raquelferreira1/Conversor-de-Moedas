9a02921d1b5de6f9240db7c5f84228c8a31cb263
raquelferreira1 <raquel.riosferreira@gmail.com>
2023-02-15 19:59:57 -0300

    consumindo API

--- scripts.js
+++ scripts.js
@@ -1,14 +1,16 @@
 const button = document.getElementById('botão-conversão') //peguei o click do botão 'converter'
 const select = document.getElementById('conversor-select')
 
-const dolar = 5.11
-const euro = 5.51
-const bitcoin = 0000011
+const convertValues = async () => {
+    const inputReais = document.getElementById('input-valor').value;
+    const valorReal = document.getElementById('valor-real');
+    const valorEmDolar = document.getElementById('valor-em-dolar');
 
-const convertValues = () => {
-    const inputReais = document.getElementById('input-valor').value
-    const valorReal = document.getElementById('valor-real')
-    const valorEmDolar = document.getElementById('valor-em-dolar')
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response=>response.json())
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
 
     valorReal.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
